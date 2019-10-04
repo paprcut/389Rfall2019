3ff4d5582fd6987863b0f4b0bbc11b2779152c76
Wyeth Force <wlforce@hotmail.com>
2019-10-04 02:51:50 -0400

    uploading my stub.py so I can download and test it on Kali

--- week/4/stub.py
+++ week/4/stub.py
@@ -1,37 +1,98 @@
 """
-    Use the same techniques such as (but not limited to):
-        1) Sockets
-        2) File I/O
-        3) raw_input()
+	Use the same techniques such as (but not limited to):
+	1) Sockets
+	2) File I/O
+	3) raw_input()
 
-    from the OSINT HW to complete this assignment. Good luck!
+	from the OSINT HW to complete this assignment. Good luck!
 """
 
 import socket
+import time
 
 host = "wattsamp.net" # IP address here
 port = 1337 # Port here
 
-def execute_cmd(cmd):
-    """
-        Sockets: https://docs.python.org/3/library/socket.html
-        How to use the socket s:
+global_prompt = "> "
+dir = ""
 
-            # Establish socket connection
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, port))
+def execute_cmd(cmd, prompt):
+	"""
+		Sockets: https://docs.python.org/3/library/socket.html
+		How to use the socket s:
 
-            Reading:
+		# Establish socket connection
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((host, port))
 
-                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
-                print(data)             # Prints data
+		Reading:
 
-            Sending:
+			data = s.recv(1024)     # Receives 1024 bytes from IP/Port
+			print(data)             # Prints data
 
-                s.send("something to send\n")   # Send a newline \n at the end of your command
-    """
-    print("IMPLEMENT ME")
+		Sending:
+
+ 			s.send("something to send\n")   # Send a newline \n at the end of your command
+	"""
+	if cmd == "help":
+
+		print("shell, pull <remote-path> <local-path>, help, quit, exit")
+	elif cmd == "shell":
+
+		int exit = 0
+
+		global_prompt = "/" + global_prompt
+
+		while exit = 0:
+
+			inner_cmd = input(global_prompt)
+
+			print("\n")
+
+			if inner_cmd = = ("exit" || "quit"):
+
+				exit = 1
+			else:
+
+				execute_cmd(inner_cmd, global_prompt)
+
+			print("\n")
+
+		global_prompt = "> "
+
+	else:
+
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((host, port))
+
+		time.sleep(1)
+
+
+		data = s.recv(1024)
+
+
+		s.send(cmd)
+
+		time.sleep(1)
+
+		data = s.recv(1024)
+		print(data)
 
 
 if __name__ == '__main__':
-    print("IMPLEMENT ME")
+	int exit = 0
+
+	while exit == 0:
+
+		cmd = input(global_prompt)
+
+		print("\n")
+
+		if cmd == ("exit" || "quit"):
+
+			exit = 1
+		else:
+
+			execute_cmd(cmd, global_prompt)
+
+		print("\n")
